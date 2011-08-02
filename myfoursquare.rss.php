790785bc18cd982d2c7f1423789b8cff69398f66
Chris Dzombak <chris@chrisdzombak.net>
2011-08-01 21:56:21 -0400

    fixing up proxy/4sq rss php code

--- myfoursquare.rss.php
+++ myfoursquare.rss.php
@@ -1,8 +1,10 @@
 <?php
 
+$url = 'https://feeds.foursquare.com/history/RBLD5NMIHX2KNUDT0WK2A4B3CCB0QPYM.rss';
+
 header('Content-type: application/xml');
 
-$handle = fopen($_REQUEST['url'], "r");
+$handle = fopen($url, "r");
 
 if ($handle) {
     while (!feof($handle)) {
